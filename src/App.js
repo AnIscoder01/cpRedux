5d69bede5f7e1a49e6bea29ce0a50e60b77a6224
AnIscoder01 <99267983+AnIscoder01@users.noreply.github.com>
2022-04-03 22:38:00 +0100

    'mychekpoint'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AddTodo from "./components/AddTodo";
+import ListTodos from "./components/ListTodos";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <ListTodos/>
+      <AddTodo/>
+    </div>
+  );
+}
+
+export default App;
